01286ab15c1acca1956180453aa1d0043e3139e7
Shawon Mondol Shibu <Shawonmondol72@gmail.com>
2024-08-22 02:50:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let output = document.getElementById("output");
+let input = document.getElementById("input");
+let value = 0;
+let store = localStorage;
+
+function update() {
+    submit = () => {
+    let inputValue = parseInt(input.value);
+    if (input.value == 0) {
+      store.setItem("num", 0);
+      return (input.style.border = "1px solid red");
+    } else {
+      store.setItem("num", inputValue);
+      value = parseInt(store.getItem("num"));
+    }
+    output.innerText = value;
+  };
+
+  // increment Output: 1
+  increment = () => {
+    let increase = (value += 1);
+    store.setItem("num", increase);
+    output.innerText = increase;
+  };
+
+  // reset Output: 0
+  reset = () => {
+    store.setItem("num", 0);
+    output.innerText = value = 0;
+  };
+
+  // decrement Output: -1
+  decrement = () => {
+    if (value === 0) {
+      return store.setItem("num", 0);
+    }
+    let decrease = (value -= 1);
+    store.setItem("num", decrease);
+    output.innerText = decrease;
+  };
+
+  value = parseInt(store.getItem("num"));
+  output.innerText = value;
+  console.log(value);
+}
+update();
